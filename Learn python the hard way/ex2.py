7a85c77086a1655085fc969391c41989c9e77a99
GAJAPATHI <61702865+GAJA-hash@users.noreply.github.com>
2020-03-18 16:57:56 +0530

    Create ex2.py

---
+++ Learn python the hard way/ex2.py
@@ -0,0 +1,12 @@
+# A comment is used to disable part of your program
+#print("This will not run.")
+
+print("Anything before hash will run") #and this won't print in the output.
+# Also used to comment about your program
+print('This will run.')
+print("               Atom editor and text editor do the same work.\n"
+                'when saved using .py extension\n'
+'               the colour of the program line changes \nprogram execution are same as like .txt format.\n'
+"               Atleast as far as I know.")
+
+print('hash # printing test')
