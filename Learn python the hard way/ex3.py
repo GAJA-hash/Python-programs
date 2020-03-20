f1a52c14f11d89a2f9411de9309febdf4ea46c68
GAJAPATHI <61702865+GAJA-hash@users.noreply.github.com>
2020-03-20 09:51:58 +0530

    Update ex3.py

--- Learn python the hard way/ex3.py
+++ Learn python the hard way/ex3.py
@@ -16,7 +16,7 @@ print("Is it true that 3 + 2 > 5 - 7\n",3 + 2 > 5 - 7)
 print(3%4*2)
 
 print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 + 7)
+print("What is 5 - 7?", 5 - 7)
 print("Oh, that's why those were False and True!")
 print("How about some more?")
 print("Is it greater?", 5>-2)
