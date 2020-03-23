cfb87657d7204b0853095669f521602fc4a9820b
GAJAPATHI <61702865+GAJA-hash@users.noreply.github.com>
2020-03-23 22:18:07 +0530

    Update ex10.py

--- Learn python the hard way/ex10.py
+++ Learn python the hard way/ex10.py
@@ -1,5 +1,5 @@
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split \n on a line."
 backslash_cat = "I'm \\ a \\ cat."
 
 fat_cat = """
@@ -9,7 +9,14 @@ I'll do a list:
 \t* Catnip\n\t* Grass
 """
 a=tabby_cat
-print("{}".format(a))
+print("{}".format(a)) #not working-->print("{}.format(a)") & print({}.format(a))
 print(persian_cat)
 print(backslash_cat)
 print(fat_cat)
+
+
+
+print("\a \b \v \f AT==BOY\b")
+print("\r Small boy")
+print("feae")
+print("\uafed") #Character with 16-bit hex value xxxx. print("\ufea") not working.
