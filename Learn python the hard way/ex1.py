d0a239c190c0e3ebad2c1918879c79ae670d51b1
GAJAPATHI <61702865+GAJA-hash@users.noreply.github.com>
2021-08-15 11:11:36 +0530

    Update ex1.py

--- Learn python the hard way/ex1.py
+++ Learn python the hard way/ex1.py
@@ -4,7 +4,7 @@ print("I like typing this.")
 print("This is fun.")
 print('Yay! Printing.')
 print("I'd much rather you 'not.'")
-print('I "said" do not touch this.')
+print('I "said" do not touch this. yes')
 #print("nothing dude")
 
 print("hello dude"
